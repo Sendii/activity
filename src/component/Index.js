cd951e1c38c9944f531dad89996de437d75cb248
Sendii <sendidianhadiwijaya@gmail.com>
2021-04-05 14:21:48 +0700

    adding function es6

--- src/component/Index.js
+++ src/component/Index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 class Index extends React.Component{
-	render(){
-		console.log('loaded')
-
+	constructor(props){
+		super(props)
+		this.state = {
+			counter: 0,
+			data: []
+		}		
+	}	
+	changeData = () => {
+		this.state.data.push({
+			name: 'nama 10'
+		})
+		this.setState({
+			counter: this.state.counter + 1
+		})
+		console.log(this.state.data)
+	}
+	render(){		
 		return(
 			<div className="row">
 				<input className="form-control" placeholder="Input Activity" type="text" />
-				<button className="btn-submit">Add</button>
+				<button className="btn-submit" onClick={this.changeData}>Add</button>
 			</div>
 			)
 	}
