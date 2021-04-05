175a1205619929baefa1dd4aa1af6ad13be6e2c7
Sendii <sendidianhadiwijaya@gmail.com>
2021-04-05 15:08:15 +0700

    onclick show list data

--- src/component/Index.js
+++ src/component/Index.js
@@ -4,23 +4,37 @@ class Index extends React.Component{
 		super(props)
 		this.state = {
 			counter: 0,
+			activity: '',
 			data: []
 		}		
 	}	
-	changeData = () => {
+	submitData = () => {
 		this.state.data.push({
-			name: 'nama 10'
+			name_activity: this.state.activity
 		})
 		this.setState({
+			activity: '',
 			counter: this.state.counter + 1
 		})
 		console.log(this.state.data)
 	}
-	render(){		
+	changeValue = (e) => {
+		this.setState({
+			activity: e.target.value
+		})		
+	}
+	render(){
+	const content = Object.entries(this.state.data).map((v, k) => {
+		return <li key={k}>{v[1].name_activity}</li>
+	})		
 		return(
 			<div className="row">
-				<input className="form-control" placeholder="Input Activity" type="text" />
-				<button className="btn-submit" onClick={this.changeData}>Add</button>
+				<input className="form-control" value={this.state.activity} onChange={this.changeValue} placeholder="Input Activity" type="text" />
+				<button className="btn-submit" onClick={this.submitData}>Add</button>
+
+				<ul>
+				{content}
+				</ul>
 			</div>
 			)
 	}
